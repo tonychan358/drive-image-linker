c09f3779d17c1cd667676225d3b14419c38cabf0
Lonely SIR <outlook188888@gmail.com>
2026-09-02 18:39:01 +0800

    update: 同步前端 週三 02/09/2026

--- config.example.js
+++ config.example.js
@@ -3,7 +3,7 @@
  */
 var CONFIG = {
   // Apps Script Web App 部署網址，以 /exec 結尾
-  API_URL: "about:blank",
+  API_URL: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXX/exec',
   // 同 Apps Script 專案屬性 API_TOKEN 一樣嘅字串
   TOKEN: 'change-me',
 };
