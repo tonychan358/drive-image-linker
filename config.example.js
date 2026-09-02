d70b5dbfa98271ecd5cf68468a2fa84dc3a20cf8
Lonely SIR <outlook188888@gmail.com>
2026-09-02 18:39:45 +0800

    update: 同步前端 週三 02/09/2026

--- config.example.js
+++
@@ -1,9 +0,0 @@
-/**
- * 複製呢個檔做 web/config.js 再填。config.js 已 gitignore，唔會 commit。
- */
-var CONFIG = {
-  // Apps Script Web App 部署網址，以 /exec 結尾
-  API_URL: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXX/exec',
-  // 同 Apps Script 專案屬性 API_TOKEN 一樣嘅字串
-  TOKEN: 'change-me',
-};
